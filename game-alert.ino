378bc601b96cccd838443c0165e40fc78dca4a85
Jonah Dahlquist <hi@jonah.name>
2020-04-25 12:23:22 -0700

    Added real login form populated with nearby networks

--- game-alert.ino
+++ game-alert.ino
@@ -71,6 +71,11 @@ IPAddress gateway(192,168,0,2);
 IPAddress subnet(255,255,255,0);
 
 void promptCredentials() {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int countSsids = WiFi.scanNetworks();
+  
   WiFi.softAPConfig(local_IP, gateway, subnet);
   WiFi.softAP("game-alert-device", "extremelymoist", 3, false, 1);
 
@@ -91,25 +96,78 @@ void promptCredentials() {
   dnsServer.start(53, "*", local_IP);
 
   while (true) {
-    WiFiClient client = server.available();
-    if (client) {
-      Serial.println("Got client");
-      while (client.connected()) {
-        if (client.available()) {
-          String line = client.readStringUntil('\r');
-          Serial.print(line);
-
-          if (line.length() == 1 && line[0] == '\n') {
-            // serve up the page, blink one LED like a cursor until a response is received
-            Serial.println("HTTP/1.1 200 OK\r\nContent-Type:text/html\r\nContent-length:6\r\nConnection:close\r\n\r\nHello!");
-            client.println("HTTP/1.1 200 OK\r\nContent-Type:text/html\r\nContent-length:6\r\nConnection:close\r\n\r\nHello!");
-            break;
-          }
-        }
-      }
-      delay(1);
-      client.stop();
-    }
+    routeHttpRequest(server.available(), countSsids);
     dnsServer.processNextRequest();
   }
+}
+
+void routeHttpRequest(WiFiClient client, int countSsids) {
+  if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        String requestLine = client.readStringUntil('\r');
+        if (requestLine.startsWith("GET ")) {
+          handleCaptivePortal(client, countSsids);
+        }
+        // handle other routes in ELSE statements
+        break;
+      }
+    }
+    delay(500);
+    client.stop();
+  }
+}
+
+void handleCaptivePortal(WiFiClient client, int countSsids) {
+  while (client.connected()) {
+    if (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+      if (line.length() == 1 && line[0] == '\n') {
+        // serve up the page, blink one LED like a cursor until a response is received
+        Serial.println(renderPortal(countSsids));
+        client.println(renderPortal(countSsids));
+        break;
+      }
+    }
+  }
+}
+
+String renderPortal(int countSsids) {
+  String options = String("");
+  for (int i = 0; i < countSsids; i++) {
+    Serial.println(WiFi.SSID(i));
+    options.concat("<option>" + WiFi.SSID(i) + "</option>");
+  }
+  String content =
+  String("<!DOCTYPE html>\r\n") +
+         "<html>" +
+         "<head>" +
+         "<title>WiFi Credentials</title>" +
+         "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
+         "<style>" +
+         "select,input,button {" +
+         "  width: 100%;" +
+         "  margin-bottom: 16px;" +
+         "  font-size: 20px;" +
+         "}" +
+         "</style>" +
+         "</head>" +
+         "<body>" +
+         "<h1>Enter Your WiFi Credentials</h1>" +
+         "<form method=\"post\" action=\"/\">" +
+         "<select name=\"s\">" +
+         options +
+         "</select>" +
+         "<input type=\"password\" name=\"p\">" +
+         "<button type=\"submit\">Connect</button>" +
+         "</form>" +
+         "</body>" +
+         "</html>";
+  return String("HTTP/1.1 200 OK\r\n") +
+         "Content-Type: text/html\r\n" +
+         "Content-Length: " + content.length() + "\r\n" +
+         "Connection: close\r\n" +
+         "\r\n" +
+         content;
 }
